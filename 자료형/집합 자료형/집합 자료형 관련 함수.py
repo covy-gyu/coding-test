2f66603b7110c58c3081f6c3349dac29ec6b31e8
guchur <74587816+gozldwnd1eo@users.noreply.github.com>
2022-10-06 14:41:05 +0900

    [Chore] 분리 및 정리

---
+++ 자료형/집합 자료형/집합 자료형 관련 함수.py
@@ -0,0 +1,12 @@
+# 집합 자료형 관련 함수
+data = set([1,2,3])
+print(data)
+#새로운 원소 추가
+data.add(4)
+print(data)
+#새로운 원소 여러 개 추가
+data.update([5,6])
+print(data)
+#특정한 값을 갖는 원소 삭제
+data.remove(3)
+print(data)
