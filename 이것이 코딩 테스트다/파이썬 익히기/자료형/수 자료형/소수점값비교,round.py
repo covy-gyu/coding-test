c992e9d7fb657c5e058797a3a40dd59592a85875
guchur <74587816+gozldwnd1eo@users.noreply.github.com>
2022-10-17 16:02:09 +0900

    [Mod] add round

---
+++ 이것이 코딩 테스트다/파이썬 익히기/자료형/수 자료형/소수점값비교,round.py
@@ -0,0 +1,15 @@
+a = 0.3 + 0.6
+print(a)
+
+if a == 0.9:
+    print(True)
+else:
+    print(False)
+
+#반올림 round(소수점숫자,소스점이하 표현할 자리 개수)
+print(round(a,4))
+
+if round(a,4) == 0.9:
+    print(True)
+else:
+    print(False)
