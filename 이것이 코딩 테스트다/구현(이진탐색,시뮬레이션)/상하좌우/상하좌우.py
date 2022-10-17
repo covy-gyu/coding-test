cb306776364387f98fd814e14907c4afe015984d
guchur <74587816+gozldwnd1eo@users.noreply.github.com>
2022-10-17 17:14:47 +0900

    [Add] 상하좌우

--- 이것이 코딩 테스트다/구현(이진탐색,시뮬레이션)/상하좌우/상하좌우.py
+++ 이것이 코딩 테스트다/구현(이진탐색,시뮬레이션)/상하좌우/상하좌우.py
@@ -1,5 +1,21 @@
 #nxn 정사각형 여행자의 이동 순서대로 도착하게 되는 곳의 좌표 구하기
 n = int(input())
+direction = list(map(str,input()))
+x =0 
+y =0
 
-list = [[0 for j in range(n)] for i in range(n)]
-print(list)
+arr = [[0 for j in range(n)] for i in range(n)]
+
+for i in range(len(direction)):
+    d = direction[i]
+    if d == "L" and (x-1)>=0:
+        x-=1
+    elif d == "R" and (x+1)<len(direction):
+        x+=1
+    elif d == "U" and (y-1)>=0:
+        y-=1
+    elif d == "D" and (y+1)<len(direction):
+        y+=1
+    else: continue
+
+print(x,y)
