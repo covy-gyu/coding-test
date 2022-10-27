e6c6865debea112269c2ef476ccbc3757a60c1a0
guchur <74587816+gozldwnd1eo@users.noreply.github.com>
2022-10-27 12:28:56 +0900

    [Add] 빠르게 입력 받기 - readline().rstrip()

---
+++ 이것이 코딩 테스트다/이진 탐색/빠르게 입력받기 - readline rstrip.py
@@ -0,0 +1,8 @@
+# 데이터의 개수가 1,000만 개를 넘어가거나 탐색 범위의 크기가 1,000억 이상이라면 이진 탐색 알고리즘을 의심해보자 #
+
+import sys
+#하나의 문자열 데이터 입력받기
+input_data = sys.stdin.readline().rstrip()
+
+#입력받은 문자열 그대로 출력
+print(input_data)
