7370b4c310da2639d912208329f798c49480a6eb
guchur <74587816+gozldwnd1eo@users.noreply.github.com>
2022-10-27 18:37:50 +0900

    [Mod] 떡 길이 - 이진탐색(파라메트릭 서치)

--- 이것이 코딩 테스트다/이진 탐색/문제/떡볶이 떡 만들기.py
+++ 이것이 코딩 테스트다/이진 탐색/문제/떡볶이 떡 만들기.py
@@ -3,25 +3,30 @@
 # 적절한 높이를 찾을 때까지 절단기의 높이 H를 반복해서 조정
 # 입력이 10억까지 > 바로 이진탐색
 
-def binary_search(arr,target, start, end):
-    while start<=end:
-        mid = (start+end)//2
-        if (sum(arr) - len(arr)*mid) == target:
-            return mid
-        elif (sum(arr) - len(arr)*mid) > target:
-            end = mid - 1
-        else:
-            start = mid +1
-    while target != arr[mid]:
-        if target < arr[mid]:
-            arr[mid] -= 1
-        elif target > arr[mid]:
-            arr[mid] += 1
-    return mid
+#떡의 개수(N)와 요청한 떡의 길이(M)을 입력받기
+n,m = list(map(int,input().split(' ')))
+#각 떡의 개별 높이 정보를 입력받기
+arr = list(map(int, input().split()))
 
-n,m = map(int,input().split())
+#이진 탐색을 위한 시작점과 끝점 설정
+start =0
+end = max(arr)
 
-arr = list(map(int,input().split()))
-arr.sort()
+#이진 탐색 수행(반복적)
+result =0
+while(start <= end):
+    total =0
+    mid = (start+end)//2
+    for x in arr:
+        #잘랐을 때의 떡의 양 계산
+        if x>mid:
+            total += x - mid
+    #떡의 양이 부족한 경우 더 많이 자르기(왼쪽 부분 탐색)
+    if total < m:
+        end = mid -1
+    #떡의 양이 충분한 경우 덜 자르기(오른쪽 부분 탐색)
+    else:
+        result = mid #최대한 덜 잘랐을 때가 정답이므로, 여기에서 result에 기록
+        start = mid+1
 
-print(arr[binary_search(arr,m,0,n-1)])
+print(result)
